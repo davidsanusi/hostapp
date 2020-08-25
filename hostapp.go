abaf2346ca1b9af65a4efcbfcbf809d8968563af
David Sanusi <sanusidavid@gmail.com>
2020-08-25 23:39:32 +0100

    Added more endpoints

--- hostapp.go
+++ hostapp.go
@@ -7,15 +7,41 @@ import (
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	myHost, err := os.Hostname() // Here we decided to explicitly handle all the return values
+// 	if err != nil {
+// 		log.Fatal(err) // Send error message to log output
+// 	}
+// 	fmt.Fprintln(w, "Greetings World WFH, 世界. You are connected to host", myHost)
+// }
+
+// func main() {
+// 	http.HandleFunc("/", handler)
+// 	log.Fatal(http.ListenAndServe(":8000", nil))
+// }
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	myHost, err := os.Hostname() // Here we decided to explicitly handle all the return values
 	if err != nil {
 		log.Fatal(err) // Send error message to log output
 	}
 	fmt.Fprintln(w, "Greetings World WFH, 世界. You are connected to host", myHost)
-}
+    })
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello endpoint")
+    })
+
+    http.HandleFunc("/yes", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Yes endpoint")
+    })
+
+    http.HandleFunc("/amen", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Amen endpoint")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
